4815db1dc571abf28565dca714d57a874695062b
Mano <apple@Macintosh.local>
2021-03-08 21:49:20 +0300

    finished tasks 2 and 3 of Express

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require("express");
-const products = require("./data");
+let products = require("./data");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const slugify = require("slugify");
 
 const app = express();
 
 app.use(cors());
 
+app.use(bodyParser.json());
+
 app.get("/products", (req, res) => {
     res.json(products);
+});
+
+app.delete("/products/:productId", (req, res) => {
+    const { productId } = req.params;
+    const foundProduct = products.find((product) => product.id === +productId);
+    if (foundProduct) {
+        products = products.filter((product) => product.id !== +productId);
+        res.status(204).end();
+    } else {
+        res.status(404).json({ message: "Product not found"});
+    }
+});
+
+app.post("/products", (req, res) => {
+    const id = products[products.length - 1].id + 1;
+    const slug = slugify(req.body.name, { lower: true });
+    const newProduct = { id, slug, ...req.body };
+    products.push(newProduct);
+    res.status(201).json(newProduct);
+    
 });
 
 app.listen(8000);
--- data.js
+++ data.js
@@ -1,11 +1,13 @@
 const products =  [
     {
+        id: 1,
         name: "Express Delivery",
         price: "SR 299",
         image: "<a href='https://www.freepik.com/vectors/technology'>Technology vector created by grmarc - www.freepik.com</a>",
     },
 
     {
+        id: 2,
         name: "Normal Delivery",
         price: "SR 199",
         image: "<a href='https://www.freepik.com/vectors/car'>Car vector created by rawpixel.com - www.freepik.com</a>",
